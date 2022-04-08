abef1330792d66287d8af0ce65d1a38f2a758e77
Christian Provenzano <18606244+caproven@users.noreply.github.com>
2022-04-07 21:35:29 -0400

    chore: cobra scaffolding

--- main.go
+++ main.go
@@ -1,29 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-)
+import "github.com/caproven/termdict/cmd"
 
 func main() {
-	var word string
-
-	flag.StringVar(&word, "w", "", "word to define")
-	flag.Parse()
-
-	if word == "" {
-		fmt.Println("Must provide word")
-		os.Exit(1)
-	}
-
-	defs, err := define(word)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Definitions for %s:\n", word)
-	for _, def := range defs {
-		fmt.Printf("\t%s - %s\n", def.PartOfSpeech, def.Meaning)
-	}
+	cmd.Execute()
 }
