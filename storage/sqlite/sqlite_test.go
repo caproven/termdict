84c6b1d03c5a0dc172cd96a9a575418656090407
Christian Provenzano <18606244+caproven@users.noreply.github.com>
2026-01-23 23:44:53 -0500

    test: sqlite word/def operations

---
+++ storage/sqlite/sqlite_test.go
@@ -0,0 +1,240 @@
+package sqlite
+
+import (
+	"database/sql"
+	"io"
+	"testing"
+
+	"github.com/caproven/termdict/dictionary"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func newTestDB(t *testing.T) (_ *sql.DB) {
+	t.Helper()
+	db, err := sql.Open("sqlite3", ":memory:")
+	require.NoError(t, err)
+	return db
+}
+
+func closeAndAssertError(t testing.TB, closer io.Closer) {
+	assert.NoError(t, closer.Close())
+}
+
+func TestNewStore(t *testing.T) {
+	db := newTestDB(t)
+	defer closeAndAssertError(t, db)
+	_, err := NewStore(t.Context(), db)
+	require.NoError(t, err)
+
+	// Assert foreign key constraints enabled for connection
+	var foreignKeys int
+	require.NoError(t, db.QueryRowContext(t.Context(), `PRAGMA foreign_keys`).Scan(&foreignKeys))
+	assert.Equal(t, 1, foreignKeys)
+
+	// Check db setup occurred
+	var wordsCount int
+	require.NoError(t, db.QueryRowContext(t.Context(), `SELECT count() FROM words`).Scan(&wordsCount))
+	assert.Equal(t, 0, wordsCount)
+
+	var definitionsCount int
+	require.NoError(t, db.QueryRowContext(t.Context(), `SELECT count() FROM definitions`).Scan(&definitionsCount))
+	assert.Equal(t, 0, definitionsCount)
+
+	// Check migrations and setup are idempotent
+	_, err = NewStore(t.Context(), db)
+	assert.NoError(t, err)
+}
+
+func TestStore_LookupWord(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		db := newTestDB(t)
+		defer closeAndAssertError(t, db)
+		store, err := NewStore(t.Context(), db)
+		require.NoError(t, err)
+
+		_, err = db.ExecContext(t.Context(), `INSERT INTO words (word) VALUES ('foo');
+INSERT INTO definitions (word_id, definition, part_of_speech) VALUES
+(last_insert_rowid(), 'def 1', 'noun'),
+(last_insert_rowid(), 'def 2', 'adjective');`)
+		require.NoError(t, err)
+
+		defs, err := store.LookupWord(t.Context(), "foo")
+		require.NoError(t, err)
+		expectedDefs := []dictionary.Definition{
+			{PartOfSpeech: "noun", Meaning: "def 1"},
+			{PartOfSpeech: "adjective", Meaning: "def 2"},
+		}
+		assert.Equal(t, defs, expectedDefs)
+
+		// check case ignored for lookup
+		defs, err = store.LookupWord(t.Context(), "FOO")
+		require.NoError(t, err)
+		assert.Equal(t, expectedDefs, defs)
+	})
+
+	t.Run("word exists with no definitions", func(t *testing.T) {
+		db := newTestDB(t)
+		defer closeAndAssertError(t, db)
+		store, err := NewStore(t.Context(), db)
+		require.NoError(t, err)
+
+		_, err = db.ExecContext(t.Context(), `INSERT INTO words (word) VALUES ('foo')`)
+		require.NoError(t, err)
+
+		defs, err := store.LookupWord(t.Context(), "foo")
+		assert.Error(t, err)
+		assert.Len(t, defs, 0)
+	})
+}
+
+func TestStore_ContainsWord(t *testing.T) {
+	db := newTestDB(t)
+	defer closeAndAssertError(t, db)
+	store, err := NewStore(t.Context(), db)
+	require.NoError(t, err)
+
+	_, err = db.ExecContext(t.Context(), `INSERT INTO words (word) VALUES ('foo')`)
+	require.NoError(t, err)
+
+	tests := map[string]struct {
+		word    string
+		want    bool
+		wantErr bool
+	}{
+		"word exists matching case": {
+			word: "foo",
+			want: true,
+		},
+		"word exists non-matching case": {
+			word: "FOO",
+			want: true,
+		},
+		"word doesn't exist": {
+			word: "bar",
+			want: false,
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			ok, err := store.ContainsWord(t.Context(), tt.word)
+			if tt.wantErr {
+				assert.Error(t, err)
+				assert.False(t, ok)
+				return
+			}
+			assert.NoError(t, err)
+			assert.Equal(t, tt.want, ok)
+		})
+	}
+}
+
+func TestStore_SaveWord(t *testing.T) {
+	t.Run("lowercase word", func(t *testing.T) {
+		db := newTestDB(t)
+		defer closeAndAssertError(t, db)
+		store, err := NewStore(t.Context(), db)
+		require.NoError(t, err)
+
+		defs := []dictionary.Definition{
+			{PartOfSpeech: "verb", Meaning: "def 1"},
+			{PartOfSpeech: "adverb", Meaning: "def 2"},
+		}
+		err = store.SaveWord(t.Context(), "foo", defs)
+		require.NoError(t, err)
+
+		gotDefs, err := store.LookupWord(t.Context(), "foo")
+		require.NoError(t, err)
+		assert.Equal(t, defs, gotDefs)
+
+		// Ensure only a single row written to words table
+		var wordCount int
+		require.NoError(t, db.QueryRowContext(t.Context(), `SELECT count() FROM words`).Scan(&wordCount))
+		assert.Equal(t, 1, wordCount)
+
+		// Ensure only the expected rows written to definitions table
+		var defCount int
+		require.NoError(t, db.QueryRowContext(t.Context(), `SELECT count() FROM definitions`).Scan(&defCount))
+		assert.Equal(t, 2, defCount)
+	})
+
+	t.Run("uppercase word transformed to lowercase", func(t *testing.T) {
+		db := newTestDB(t)
+		defer closeAndAssertError(t, db)
+		store, err := NewStore(t.Context(), db)
+		require.NoError(t, err)
+
+		defs := []dictionary.Definition{
+			{PartOfSpeech: "conjunction", Meaning: "def 1"},
+		}
+		err = store.SaveWord(t.Context(), "BAR", defs)
+		require.NoError(t, err)
+
+		gotDefs, err := store.LookupWord(t.Context(), "bar")
+		require.NoError(t, err)
+		assert.Equal(t, defs, gotDefs)
+	})
+
+	t.Run("invalid inputs", func(t *testing.T) {
+		tests := map[string]struct {
+			word string
+			defs []dictionary.Definition
+		}{
+			"empty word": {
+				word: "",
+				defs: []dictionary.Definition{
+					{PartOfSpeech: "verb", Meaning: "def 1"},
+				},
+			},
+			"whitespace word": {
+				word: " ",
+				defs: []dictionary.Definition{
+					{PartOfSpeech: "verb", Meaning: "def 1"},
+				},
+			},
+			"no definitions": {
+				word: "foo",
+				defs: []dictionary.Definition{},
+			},
+		}
+
+		for name, tt := range tests {
+			t.Run(name, func(t *testing.T) {
+				db := newTestDB(t)
+				defer closeAndAssertError(t, db)
+				store, err := NewStore(t.Context(), db)
+				require.NoError(t, err)
+
+				err = store.SaveWord(t.Context(), tt.word, tt.defs)
+				assert.Error(t, err)
+
+				// Ensure no entries written to words table
+				var wordCount int
+				require.NoError(t, db.QueryRowContext(t.Context(), `SELECT count() FROM words`).Scan(&wordCount))
+				assert.Equal(t, 0, wordCount)
+
+				// Ensure no entries written to definitions table
+				var defCount int
+				require.NoError(t, db.QueryRowContext(t.Context(), `SELECT count() FROM definitions`).Scan(&defCount))
+				assert.Equal(t, 0, defCount)
+			})
+		}
+	})
+
+	t.Run("word already exists", func(t *testing.T) {
+		db := newTestDB(t)
+		defer closeAndAssertError(t, db)
+		store, err := NewStore(t.Context(), db)
+		require.NoError(t, err)
+
+		_, err = db.ExecContext(t.Context(), `INSERT INTO words (word) VALUES ('foo')`)
+		require.NoError(t, err)
+
+		err = store.SaveWord(t.Context(), "foo", []dictionary.Definition{
+			{PartOfSpeech: "verb", Meaning: "def 1"},
+		})
+		assert.Error(t, err)
+	})
+}
